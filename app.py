1baac7322bf87da5fb9b8479ac651e40d50e3a06
weihaomiao <weihao.sheng2019@gmail.com>
2023-01-20 01:00:55 +0000

    final,yes!

--- app.py
+++ app.py
@@ -297,5 +297,4 @@ def sell():
         stocks = db.execute("SELECT DISTINCT symbol FROM portfolio WHERE username = ?", session["username"])
         return render_template("sell.html", stocks=stocks)
 
-if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+
